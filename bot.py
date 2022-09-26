7b9eacb04546f498fbd1b4b54ddf73a668fd5588
iskandar777-dar <54578330+iskandar777-dar@users.noreply.github.com>
2022-09-26 18:12:00 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@
 import logging
 from datetime import datetime
 
-from telegram.ext import ParseMode, InlineKeyboardMarkup, \
+from telegram import ParseMode, InlineKeyboardMarkup, \
     InlineKeyboardButton
 from telegram.ext import InlineQueryHandler, ChosenInlineResultHandler, \
     CommandHandler, MessageHandler, Filters, CallbackQueryHandler
